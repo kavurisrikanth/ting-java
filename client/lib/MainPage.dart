1beea331f08465dd2e03dba4c230c180fa2537bd
Srikanth Kavuri <ksrs9312@gmail.com>
2020-11-29 17:17:33 +0530

    Added client

---
+++ client/lib/MainPage.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/widgets.dart';
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'This is a Stateless Widget.'
+    );
+  }
+}
